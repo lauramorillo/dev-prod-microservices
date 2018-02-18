a9bb65614a54e43b4d5ddb58218488e72435c117
Laura Morillo-Velarde <laura.morillovelarde@gmail.com>
2018-02-19 00:50:00 +0100

    Fixing first_app tests

--- first_app/spec/unit/jasmine_examples/PlayerSpec.js
+++ first_app/spec/unit/jasmine_examples/PlayerSpec.js
@@ -1,6 +1,7 @@
 describe("Player", function() {
-  var Player = require('../../lib/jasmine_examples/Player');
-  var Song = require('../../lib/jasmine_examples/Song');
+  var Player = require('../../../lib/jasmine_examples/Player');
+  var Song = require('../../../lib/jasmine_examples/Song');
+
   var player;
   var song;
 
