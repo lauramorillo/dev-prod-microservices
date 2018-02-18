c525cbd49f30d330036840aa87b2d350d36c37df
Laura Morillo-Velarde <laura.morillovelarde@gmail.com>
2018-02-18 23:36:48 +0100

    Adding docker and tests to first_app

---
+++ first_app/spec/e2e/ApiSpec.js
@@ -0,0 +1,14 @@
+describe("Api", function() {
+  var request = require('request')
+  
+  it("returns 200 code", function(done) {
+    request('http://localhost:3000', function(error, response, body) {
+      if (error) {
+        done.fail(error)
+        return
+      }
+      expect(response.statusCode).toBe(200);
+      done()
+    })
+  });
+});
